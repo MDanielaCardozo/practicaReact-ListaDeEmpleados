5b63c4d9247009d2e885e03c6096ee910bdb6e59
Daniela Cardozo <mdanielacardozo@gmail.com>
2022-08-31 20:32:43 -0300

    Agregué la imagen a la lista de empleados

--- src/components/EmpleadoList.js
+++ src/components/EmpleadoList.js
@@ -9,63 +9,63 @@ const EmpleadoList = () => {
       fullName: "Laya Dueñas",
       title: "CEO",
       department: "Business",
-      pic: "empleado01.png",
+      pic: "https://cdn-icons-png.flaticon.com/512/147/147136.png",
     },
     {
       id: 2,
       fullName: "Astryd Vallés",
       title: "CMO",
       department: "Marketing",
-      pic: "empleado02.png",
+      pic: "https://cdn-icons-png.flaticon.com/512/147/147136.png",
     },
     {
       id: 3,
       fullName: "Shantell Meza",
       title: "CFO",
       department: "Business",
-      pic: "empleado03.png",
+      pic: "https://cdn-icons-png.flaticon.com/512/147/147136.png",
     },
     {
       id: 4,
       fullName: "Sergio Ocampo",
       title: "CTO",
       department: "Engineering",
-      pic: "empleado04.png",
+      pic: "https://img.freepik.com/vector-premium/perfil-avatar-hombre-icono-redondo_24640-14044.jpg",
     },
     {
       id: 5,
       fullName: "Ares Jiménez",
       title: "Art Director",
       department: "Marketing",
-      pic: "empleado05.png",
+      pic: "https://cdn-icons-png.flaticon.com/512/147/147136.png",
     },
     {
       id: 6,
       fullName: "Marta Pérez",
       title: "Frontend Dev",
       department: "Engineering",
-      pic: "empleado06.png",
+      pic: "https://cdn-icons-png.flaticon.com/512/147/147136.png",
     },
     {
       id: 7,
       fullName: "Ellen Balderas",
       title: "Digital Strategist",
       department: "Marketing",
-      pic: "empleado07.png",
+      pic: "https://cdn-icons-png.flaticon.com/512/147/147136.png",
     },
     {
       id: 8,
       fullName: "Cynthia Valentín",
       title: "Backend Dev",
       department: "Engineering",
-      pic: "empleado08.png",
+      pic: "https://cdn-icons-png.flaticon.com/512/147/147136.png",
     },
     {
       id: 9,
       fullName: "Bernard Jung",
       title: "DevOps Engineer",
       department: "Engineering",
-      pic: "empleado09.png",
+      pic: "https://img.freepik.com/vector-premium/perfil-avatar-hombre-icono-redondo_24640-14044.jpg",
     },
   ]);
 
@@ -78,6 +78,7 @@ const EmpleadoList = () => {
               nombre={empleado.fullName}
               titulo={empleado.title}
               departamento={empleado.department}
+              imagen={empleado.pic}
             ></EmpleadoRow>
         })}
       </ListGroup>
--- src/components/EmpleadoRow.js
+++ src/components/EmpleadoRow.js
@@ -1,12 +1,11 @@
 import React from "react";
 import { ListGroup, Badge } from "react-bootstrap";
-import EmpleadoAvatar from "./EmpleadoAvatar";
 
 const EmpleadoRow = (props) => {
   return (
     <div>
-      <ListGroup.Item className="d-flex">
-            <EmpleadoAvatar></EmpleadoAvatar>
+      <ListGroup.Item className="d-flex my-3">
+            <img src={props.imagen} alt="avatar" className="w-25"/>
             <h1 className="text-primary display-6 m-5">{props.nombre}</h1>
             <div className="m-5">
             <p>{props.titulo}<Badge bg="info" className="m-4">{props.departamento}</Badge></p>
